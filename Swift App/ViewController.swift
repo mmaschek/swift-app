c98637de0ca0abe2c5a819e3c8c7151fb92d0de0
Mike Maschek <mmaschek@gmail.com>
2017-02-14 00:14:12 -0600

    changed comment to test

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -3,16 +3,34 @@
 //  Swift App
 //
 //  Created by Mike Maschek on 2/9/17.
-//  Copyright © 2017 Mike Maschek. All rights reserved.
+//  Copyright © 2017 Mike Maschek. All rights reserved..
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        theLabel.text = "Mikey"
+        tapCount = tapCount + 1
+        if tapCount >= 10 {
+            theLabel.text = "you have pressed more than 10 times"
+        }
+    }
+    
+    @IBAction func tapCool(_ sender: Any) {
+        theLabel.text = "Buttons are cool!"
+        print("Button tapped")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
